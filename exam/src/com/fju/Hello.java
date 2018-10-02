044d27511d7d199874b125ab59ec47154e511300
user <user@es302n05>
2018-10-02 11:00:10 +0800

    5more

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,7 +4,14 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		int age = 20;
+		String firstName = "charlotte";
+		String lastName = "Lin";
+		int hour = 10, minute = 58;
+		System.out.println(age);
+		System.out.println(firstName);
+		System.out.println(" ");
+		System.out.println(lastName);
 	}
 
 }
